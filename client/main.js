7eb9dd06a29034198cbb2ed8c8925f1b0c913f43
sanjeetSangam <ranjeetraj44666@gmail.com>
2023-04-29 21:15:05 +0530

    updated link

--- client/main.js
+++ client/main.js
@@ -96,7 +96,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch message from server -> bot chat response
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://gapsap.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
